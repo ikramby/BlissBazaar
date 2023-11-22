9ad9f8857b47c4184df271a924ddc8ddefe1e659
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-11-22 22:29:34 +0100

    first steps on Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/home';
+import HomePage from './pages/HomePage';
 import SignUp from './pages/register';
 import SignInSide from './pages/login';
 
@@ -14,7 +14,7 @@ function App() {
        
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignInSide />} />
         <Route path="/about-us" element={<AboutUs />} /> 
