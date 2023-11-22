36a01cf143b528f9dfbb229561fdb7bbee154100
ikramby <ikram@polyglot-digital.com>
2023-11-22 19:15:53 +0100

    Add About US

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
 import SignUp from './pages/register';
 import SignInSide from './pages/login';
+
 import SearchAppBar from './component/navbar'
 
 function App() {
@@ -10,11 +11,14 @@ function App() {
     <Router>
       <nav>
        <SearchAppBar />
+       
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignInSide />} />
+        <Route path="/about-us" element={<AboutUs />} /> 
+
       </Routes>
     </Router>
   );
