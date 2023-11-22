77356bc9525faf4c8b03105621c5e50f04918651
tarekHaker <tarekbnzaied@gmail.com>
2023-11-22 17:01:08 +0400

    signin and signup with database

---
+++ backend/models/index.js
@@ -0,0 +1,23 @@
+
+
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require('./user')(sequelize, DataTypes);
+
+module.exports = db;
