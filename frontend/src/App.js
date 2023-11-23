c9d8a9cff7594debfb412afdddb9bedfe917ecdf
tarekHaker <tarekbnzaied@gmail.com>
2023-11-23 14:26:18 +0400

    fixpblem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/home';
+// import  HomePage from './pages/HomePage';
 import SignUp from './pages/register';
 import SignInSide from './pages/login';
 import SearchAppBar from './component/navbar'
@@ -12,7 +12,7 @@ function App() {
        <SearchAppBar />
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignInSide />} />
       </Routes>
