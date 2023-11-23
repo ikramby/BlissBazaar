47b3a4624f8ecc9ad668ad2f0faae252265f42fc
AnisJemail <anis@polyglotanis@polyglot>
2023-11-23 18:54:30 +0100

    add the front for page EditProfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SignUp from './pages/register';
-import SignInSide from './pages/login';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUp from "./pages/register";
+import SignInSide from "./pages/login";
+import React from "react";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/register">Register</Link>
         <Link to="/login">Login</Link>
+        <Link to="/edit-profile">Edit Profile</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignInSide />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
     </Router>
   );
