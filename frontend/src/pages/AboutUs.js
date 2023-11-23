bc0ee076b51841ffc010d0bd0ba99043577828b1
ikramby <ikram@polyglot-digital.com>
2023-11-23 11:02:33 +0100

    update About Us page

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -17,16 +17,17 @@ import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper'; // Updated import
 import './AboutUs.css';
+import CardMedia from '@mui/material/CardMedia';
 
 
 const AboutUs = () => {
     
 function Copyright(props) {
     return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      <Typography variant="body2" color="white" align="center" {...props}>
         {'Copyright © '}
         <Link color="inherit" href="https://mui.com/">
-          Your Website
+        TechBazaar
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
@@ -98,7 +99,8 @@ function Copyright(props) {
       description: ['Privacy policy', 'Terms of use'],
     },
   ];
-  
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   // TODO remove, this demo shouldn't need to reset the theme.
   const defaultTheme = createTheme();
   
@@ -158,31 +160,24 @@ function Copyright(props) {
         style={{ color: 'white' }}
         gutterBottom
       >
-        Who We Are
+         <h1 className="section-title">
+         Who We Are
+         </h1>
+       
       </Typography> 
       <Typography variant="body1" align="center" paragraph>
-
+      <p className="section-paragraph">
         TechBazar is your one-stop destination for all your IT needs. We specialize in offering a wide range of top-notch IT equipment, including laptops and desktop PCs, tablets, smartphones, and cameras. With the convenience of online ordering, TechBazar makes it easy for you to find and purchase the latest and most reliable technology products. Explore our diverse selection and stay ahead in the world of digital innovation with TechBazar.
+     </p>
       </Typography>
     </Container><br></br><br></br><br></br><br></br>
       {/* End hero unit */}
      
 
  
-   
-   
-
-   
-
-    
 
 
-
-   
-
-
-      <div className="section section-left">
-        <div className="section-content">
+      <div className="section section-right" >        <div className="section-content">
           <h1 className="section-title">What We Do</h1>
           <p className="section-paragraph">
           At TechBazar, we specialize in providing cutting-edge solutions for your computer hardware needs. Our dedicated team is committed to offering a curated selection of top-quality components, ensuring that you have access to the latest and most reliable hardware. Whether you're building a high-performance PC or upgrading your existing system, we've got you covered. Explore our extensive range of processors, graphics cards, motherboards, and more, and experience the power of precision and performance with TechBazar.          </p>
@@ -221,103 +216,54 @@ function Copyright(props) {
 
       We believe in the entrepreneurial spirit of our makers, offering a seamless and supportive environment to connect with tech enthusiasts worldwide. Join TechBazar as a seller, and let your creations become part of the digital revolution, shaping the future of IT excellence.</Typography>
       </Container>
-        <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={tier.title}
-              xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
-              md={4}
-            >
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box
+      
+      <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="div"
                     sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'baseline',
-                      mb: 2,
+                     
+                      pt: '100%',
                     }}
-                  >
-                  </Box>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button fullWidth variant={tier.buttonVariant}>
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-     
+                    image="https://source.unsplash.com/random?wallpapers"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                 
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      
+      {/* Footer */}
+      <Box  component="footer">
+        
+        <Typography
+          variant="subtitle1"
+          align="center"
+         // color="text.secondary"
+          style={{ color: 'white' }}
 
-
-
-     {/* Footer */}
-
-     <Container
-        maxWidth="md"
-        component="footer"
-        sx={{
-          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-          mt: 8,
-          py: [3, 6],
-        }}
-      >
-        <Grid container spacing={4} justifyContent="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="text.primary" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href="#" variant="subtitle1" color="text.secondary">
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-
-     
+          component="p"
+        >
+ TechBazaar. All rights reserved.
+         </Typography>
+        <Copyright />
+      </Box><br></br>
       {/* End footer */}
     </div>
     
