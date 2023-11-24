dc16cad4b99cc848e7a683c85c112499419dca68
ikramby <ikram@polyglot-digital.com>
2023-11-24 09:26:07 +0100

    fix bug home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,111 +1,81 @@
 import React from 'react';
-import '../index.css'
 import {
- Typography,
- Grid,
- Button,
-} from '@material-ui/core';
-import Link from '@mui/material/Link';
-import { makeStyles } from '@material-ui/core/styles';
+  Typography,
+  Grid,
+  Button,
+  Paper,
+  Link,
+} from '@mui/material';
+import { styled } from '@mui/system';
 
-const useStyles = makeStyles((theme) => ({
-  navbar: {
-    backgroundColor: '#2e2e2e',
-  },
-  searchBar: {
-    flexGrow: 1,
-    padding: theme.spacing(1),
-    marginLeft: theme.spacing(2),
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
+// Define the styled components
+const StyledButton = styled(Button)({
+  margin: theme => theme.spacing(1),
+});
 
+const StyledTypography = styled(Typography)({
+  // Your styles for Typography component
+});
+
+const StyledLink = styled(Link)({
+  // Your styles for Link component
+  color: 'white',  // Example: Override color for Link
+});
+
+// Your functional component
 const HomePage = () => {
-  const classes = useStyles();
-
   return (
     <div>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          <Button variant="contained" color="primary" className={classes.button}>
+          <StyledButton variant="contained" color="primary">
             Main Collection
-          </Button>
+          </StyledButton>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Button variant="contained" color="primary" className={classes.button}>
+          <StyledButton variant="contained" color="primary">
             View All Products
-          </Button>
+          </StyledButton>
         </Grid>
         <Grid item xs={12}>
           <div style={{ marginTop: "50px", textAlign: "center" }}>
-            <Typography variant="h4" component="h4">
+            <StyledTypography variant="h4" component="h4">
               Welcome to TechBazaar!
-            </Typography>
-            <Typography variant="body1" component="p">
-            TechBazar is your one-stop destination for all your IT needs. We specialize in offering a wide range of top-notch IT equipment, inclu...
-            </Typography>
-            <Button variant="contained" color="primary" className={classes.button}>
-            <Link href='/AboutUs' sx={{color:'white'}}>
-            About Us!
-            </Link>
-            </Button>
+            </StyledTypography>
+            <StyledTypography variant="body1" component="p">
+              TechBazar is your one-stop destination for all your IT needs. We specialize in offering a wide range of top-notch IT equipment, inclu...
+            </StyledTypography>
+            <StyledButton variant="contained" color="primary">
+              <StyledLink href='/AboutUs'>
+                About Us!
+              </StyledLink>
+            </StyledButton>
           </div>
         </Grid>
         <Grid item xs={12}>
           <div style={{ marginTop: "50px", textAlign: "center" }}>
-            <Typography variant="h4" component="h4">
+            <StyledTypography variant="h4" component="h4">
               Products..
-            </Typography>
-            <Typography variant="body1" component="p">
-            showing products..
-            </Typography>
-            <Button variant="contained" color="primary" className={classes.button}>
+            </StyledTypography>
+            <StyledTypography variant="body1" component="p">
+              showing products..
+            </StyledTypography>
+            <StyledButton variant="contained" color="primary">
               See All Products
-            </Button>
+            </StyledButton>
           </div>
         </Grid>
         <Grid item xs={12}>
           <div style={{ marginTop: "50px", textAlign: "center" }}>
-            <Typography variant="h4" component="h4">
-              Need Some Helpe!
-            </Typography>
-            <Typography variant="body1" component="p">
-            If you have any questions or concerns, please call us on 01481 238565 between 09.00 and 16.30. For general enquiries, you can also email health@msg.gg.
-            </Typography>
-            <Button variant="contained" color="primary" className={classes.button}>
+            <StyledTypography variant="h4" component="h4">
+              Need Some Help!
+            </StyledTypography>
+            <StyledTypography variant="body1" component="p">
+              If you have any questions or concerns, please call us on 01481 238565 between 09.00 and 16.30. For general enquiries, you can also email health@msg.gg.
+            </StyledTypography>
+            <StyledButton variant="contained" color="primary">
               About Us
-            </Button>
+            </StyledButton>
           </div>
         </Grid>
       </Grid>
