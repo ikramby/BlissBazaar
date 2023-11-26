e1420ee10f42befdb56e91a932126d4261efc34e
ikramby <ikram@polyglot-digital.com>
2023-11-26 12:12:11 +0100

    add filter by category

--- backend/route/product.js
+++ backend/route/product.js
@@ -7,5 +7,7 @@ router.get('/:id', productController.getProductById);
 router.post('/', productController.createProduct);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
+router.get('/category/:category', productController.getProductsByCategory);
+
 
 module.exports = router;
