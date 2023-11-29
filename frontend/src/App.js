bc5de1e8ec7a125ec16889900ea6d472e2476191
ikramby <ikram@polyglot-digital.com>
2023-11-29 19:23:03 +0100

    reset code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SignUp from './pages/register';
-import SignInSide from './pages/login';
-import EditProfile from './pages/EditProfile';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUp from "./pages/register";
+import SignInSide from "./pages/login";
+import React from "react";
+import EditProfile from "./pages/EditProfile";
 import AboutUs from './pages/AboutUs';
 import PrivacyPolicy from './pages/PrivacyPolicy';
-import FAQ from './pages/faq';
 import TermOfUse from './pages/TermOfUse'; 
 import NewProductForm from './pages/Newproduct';
 import SearchAppBar from './component/navbar';
 import { AuthProvider } from './component/AuthContext';
 import AllProduct from './pages/allProduct';
-import BasketCard from "./pages/basketCard";
-import MediaCustomer from "./component/mediaCustomer";
-import FeaturesPage from "./pages/feature";
-import PartnerWithUsPage from './pages/partner';
-import OurTeamPage from './pages/teams';
-
 import EditProductForm from './pages/Editproduct';
-import Dashboard from './pages/dashboard';
-import Seller from './pages/seller';
-import ProtectedRoute from './component/ProtectedRoute'; // Import ProtectedRoute
-
+import Dashboard from './pages/dashboard'
+import Seller from "./pages/seller";
+import MediaCustomer from "./component/mediaCustomer";
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <nav>
-          <SearchAppBar />
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/login" element={<SignInSide />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-          <Route path="/TermOfUse" element={<TermOfUse />} />
-          <Route path="/allproduct" element={<AllProduct />} />
-          <Route path="/basket" element={<BasketCard />} />
+    <Router>
+      <nav>
+       <SearchAppBar />
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/login" element={<SignInSide />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/NewProduct" element={<NewProductForm />} />
+        <Route path="/EditProduct/:productId" element={<EditProductForm />} />
 
-          {/* Protected Routes */}
-          <Route path="/dashboard" element={ <Dashboard /> } />
-          <Route path="/seller" element={<ProtectedRoute element={<Seller />} allowedRoles={['seller']} />} />
-          <Route path="/NewProduct" element={<ProtectedRoute element={<NewProductForm />} allowedRoles={['seller']} />} />
-          <Route path="/EditProduct/:productId" element={<ProtectedRoute element={<EditProductForm />} allowedRoles={['seller']} />} />
-          <Route path="/edit-profile" element={<ProtectedRoute element={<EditProfile />} allowedRoles={['user', 'seller']} />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/seller" element={<Seller />} />
 
-          {/* Add other routes as needed */}
-        </Routes>
-      </Router>
+        <Route path="/allproduct" element={<AllProduct />} />
+        <Route path="/products/:productId" element={<MediaCustomer />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/TermOfUse" element={<TermOfUse />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+
+        TermsOfUse
+      </Routes>
+    </Router>
     </AuthProvider>
   );
 }
