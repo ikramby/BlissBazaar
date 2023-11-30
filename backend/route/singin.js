f31bdf8f0d727a7128edfd78688385ee27bdf617
tarekHaker <tarekbnzaied@gmail.com>
2023-11-30 22:58:16 +0400

    dashboard user

--- backend/route/singin.js
+++ backend/route/singin.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const router=express.Router();
-const {register,login, verifyToken, getMyInformation, updateUserInfo}=require("../controlers/controlers")
+const {register,login, verifyToken, getMyInformation, updateUserInfo, getAllUsers}=require("../controlers/controlers")
 
 
 router.post("/register",register);
@@ -8,6 +8,9 @@ router.post('/login',login);
 router.get('/verifyToken' , verifyToken)
 router.get('/getMyInformation/:email', getMyInformation);
 router.put('/updateUserInfo/:email', updateUserInfo);
+router.get('/getAllUsers' , getAllUsers)
+
+
 
 
 
---
+++ frontend/src/component/dashboardUser.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useStepperContext } from '@mui/material';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+
+
+export default function DenseTable({users}) {
+   
+  return (
+    <TableContainer component={Paper} style={{background:'transparent', marginTop:'20vh', color:'white'}}>
+     <h1>Dashboard User</h1>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table" >
+        <TableHead>
+          <TableRow >
+            <TableCell style={{ color:'white'}}>USER NAME</TableCell>
+            <TableCell align="right" style={{ color:'white'}}>Last Name</TableCell>
+            <TableCell align="right" style={{ color:'white'}}>E-mail</TableCell>
+            <TableCell align="right" style={{ color:'white'}}>Purpose</TableCell>
+            <TableCell align="right" style={{ color:'white'}}>Ban</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow
+              key={user.firstNme}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" style={{ color:'white'}} >
+                {user.firstName}
+              </TableCell>
+              <TableCell align="right" style={{ color:'white'}}>{user.lastName}</TableCell>
+              <TableCell align="right" style={{ color:'white'}}>{user.email}</TableCell>
+              <TableCell align="right" style={{ color:'white'}}>{user.purpose}</TableCell>
+              <TableCell align="right" style={{ color:'white'}}><button>Delete</button></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
