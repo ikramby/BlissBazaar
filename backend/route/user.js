9edf127833085725aa49d2545cceea1ea1889b13
Anis-Jemail <jemail.anis@gmail.com>
2023-12-02 20:54:59 +0100

    update the route and fix some bugs

--- backend/route/user.js
+++ backend/route/user.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../controlers/controlers");
+//const { verifyToken } = require("../controlers/controlers");
 const { getUserData, updateUserData } = require("../controlers/userController");
 
-router.get("/edit-profile/:userId", verifyToken, getUserData);
-router.put("/edit-profile/:userId", verifyToken, updateUserData);
+router.get("/edit-profile/:email", getUserData);
+router.put("/edit-profile/:email", updateUserData);
 
 module.exports = router;
